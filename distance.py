3de14f567e730efdbd427380da914ab9c307a38c
Oliver Seemann <oseemann@gmail.com>
2018-09-28 21:57:26 +0200

    Add implementation with linear complexity
    Tests are checking both implementations.

--- distance.py
+++ distance.py
@@ -2,7 +2,7 @@ import sys
 import unittest
 
 
-def find_shorttest_distance(doc: str, a: str, b: str) -> int:
+def find_shorttest_distance(doc: str, a: str, b: str, slow: bool = False) -> int:
     """
     Return number of words in the document `doc` that occur between the words
     `a` and `b`.
@@ -23,30 +23,86 @@ def find_shorttest_distance(doc: str, a: str, b: str) -> int:
     # Split doc into list of words
     words = doc_alnum.split()
 
-    # Lists to store positions of search arguments a and b
+    if slow:
+        return find_quadratic(words, a.lower(), b.lower())
+    else:
+        return find_linear(words, a.lower(), b.lower())
+
+
+def find_quadratic(words: str, a: str, b: str):
+    """
+    Sub-optimal implementation of the distance search.
+
+    It generates a list of all search word positions and then finds the
+    shortest difference of all position combinations.
+
+    For search words that appear only infrequently in the document this is not
+    a problem: the position lists will be short.
+    However when both search words appear very frequently, the complexity of
+    this function becomes quadratic (of the search word frequency, not the
+    number of total words).
+    """
+    # Lists to store positions of search words a and b
     pos_a = []
     pos_b = []
-    low_a = a.lower()
-    low_b = b.lower()
 
     # Find where both words appear in the doc
     for i in range(len(words)):
-        if words[i] == low_a:
+        if words[i] == a:
             pos_a.append(i)
-        elif words[i] == low_b:
+        elif words[i] == b:
             pos_b.append(i)
 
     # Calculate distances for all found combinations of positions
-    distances = [abs(pb - pa) - 1 for pa in pos_a for pb in pos_b]
+    # For search words with very high frequency in the document this will be
+    # slow, as the product of all position pairs is generated.
+    distances = [abs(pa - pb) - 1 for pa in pos_a for pb in pos_b]
 
     # Return minimum distance
     return min(distances) if distances else None
+
+
+def find_linear(words: str, w1: str, w2: str):
+    """
+    An implementation with linear complexity that iterates over all words only
+    once.
+    """
+    mindist = None
+    pos_w1 = None
+    pos_w2 = None
+    for i in range(len(words)):
+        w = words[i]
+
+        # Update last known positions of the search words
+        if w == w1:
+            pos_w1 = i
+        elif w == w2:
+            pos_w2 = i
+        else:
+            # Not a search word, skip
+            continue
+
+        if pos_w1 is None or pos_w2 is None:
+            # Have not yet found both words
+            continue
+
+        # When this is reached, a new occurence of a search word has been
+        # found. Let's calculate the distance.
+        dist = abs(pos_w1 - pos_w2) - 1
+
+        # Is this distance shorter than the one we have found so far?
+        if mindist is None or dist < mindist:
+            # Yes, remember this one
+            mindist = dist
+
+    return mindist
 
 
 class Test(unittest.TestCase):
 
     def t(self, doc, a, b, dist):
         self.assertEqual(find_shorttest_distance(doc, a, b), dist)
+        self.assertEqual(find_shorttest_distance(doc, a, b, slow=True), dist)
 
     def test_simple(self):
         self.t('a', 'a', 'a', None)
